c0ded7a59fcd5958b14eb63eff9c8bfc8894860d
Abanoub Samir <66217878+ProfAbanoub@users.noreply.github.com>
2022-06-24 12:07:43 +0200

    Time: 19 ms (22.69%), Space: 44.6 MB (57.30%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x == 0) {
+            return true;
+        }
+        if(x < 0 || x%10 == 0)
+            return false;
+        
+        int temp = 0;
+        int preX = x;
+        while (x > temp) {
+            int pop = x%10;
+            preX = x;
+            x /= 10;
+
+            temp = temp*10 + pop;
+        }
+        if(x == temp || preX == temp)
+            return true;
+        else
+            return false;
+    }
+}
