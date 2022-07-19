6fc6eb4c94d4a660f3405e523748cd9a2a302ae5
Abanoub Samir <66217878+ProfAbanoub@users.noreply.github.com>
2022-07-19 04:05:00 +0200

    Time: 248 ms (86.51%), Space: 41.8 MB (88.36%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.kt
@@ -0,0 +1,57 @@
+class Solution {
+    fun isValidSudoku(board: Array<CharArray>?): Boolean {
+        if (board == null || board.size != 9 && board[0].size != 9) {
+            return false
+        }
+
+        val mapRow = mutableMapOf<Char, Boolean>()
+        val mapColumn = mutableMapOf<Char, Boolean>()
+        for (i in 0..8) {
+            for (j in 0..8) {
+                if (board[i][j] in '1'..'9') {
+                    if (mapRow.getOrDefault(board[i][j], false)) {
+                        return false
+                    } else {
+                        mapRow.put(board[i][j], true)
+                    }
+                } else if (board[i][j] != '.') {
+                    return false
+                }
+
+                if (board[j][i] in '1'..'9') {
+                    if (mapColumn.getOrDefault(board[j][i], false)) {
+                        return false
+                    } else {
+                        mapColumn.put(board[j][i], true)
+                    }
+                } else if (board[j][i] != '.') {
+                    return false
+                }
+            }
+            mapRow.clear()
+            mapColumn.clear()
+        }
+
+        val mapBlock = mutableMapOf<Char, Boolean>()
+        for (i in 0..8 step 3) {
+            for (j in 0..8 step 3) {
+                for (m in 0..2) {
+                    for (n in 0..2) {
+                        if (board[i + m][j + n] in '1'..'9') {
+                            if (mapBlock.getOrDefault(board[i + m][j + n], false)) {
+                                return false
+                            } else {
+                                mapBlock.put(board[i + m][j + n], true)
+                            }
+                        } else if (board[i + m][j + n] != '.') {
+                            return false
+                        }
+                    }
+                }
+                mapBlock.clear()
+            }
+        }
+
+        return true
+    }
+}
